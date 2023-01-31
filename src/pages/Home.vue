621766ac3ed128f67fc0bdc96cbf2a9538eedf21
Ana Clara Mendes Bull <anaclaramendesbull@gmail.com>
2023-01-30 22:34:13 -0300

    up

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,12 +1,14 @@
 <template>
-    <pre>{{ meals }}</pre>
+    <div class="flex p-8 justify-center">
+        <input type="text" class="rounded border-2 border-gray-200 w-64" placeholder="Procurar...">
+    </div>
 </template>
 
 <script setup>
-    import { computed } from 'vue';
-    import store from '../store';
+import { computed } from 'vue';
+import store from '../store';
 
 
-    const meals = computed(() => store.state.meals)
-    
+const meals = computed(() => store.state.meals)
+
 </script>
